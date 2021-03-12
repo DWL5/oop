796e472f56c1fe1728279ab7b345cbf7b885bf59
DWL5 <ssop6403@gmail.com>
2021-03-12 22:35:43 +0900

    #결합도 - 입출금 내역을 파싱하는 인터페이스 정의

--- src/main/java/BankStatementCSVParser.java
+++ src/main/java/BankStatementCSVParser.java
@@ -3,12 +3,13 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BankStatementCSVParser {
+public class BankStatementCSVParser implements BankStatementParser {
     private static final DateTimeFormatter DATE_PATTERN =
             DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-    private BankTransaction parseFromCSV(final String line) {
-        final String[] columns  = line.split(",");
+    @Override
+    public BankTransaction parseFrom(String line) {
+        final String[] columns = line.split(",");
 
         final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
         final double amount = Double.parseDouble(columns[1]);
@@ -17,10 +18,11 @@ public class BankStatementCSVParser {
         return new BankTransaction(date, amount, description);
     }
 
-    public List<BankTransaction> parseLinesFromCSV(final List<String> lines) {
+    @Override
+    public List<BankTransaction> parseLineFrom(List<String> lines) {
         final List<BankTransaction> bankTransactions = new ArrayList<>();
-        for (final String line: lines) {
-            bankTransactions.add(parseFromCSV(line));
+        for (final String line : lines) {
+            bankTransactions.add(parseFrom(line));
         }
 
         return bankTransactions;
